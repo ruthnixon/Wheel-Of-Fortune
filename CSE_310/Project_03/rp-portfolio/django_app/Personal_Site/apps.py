dc85b68225ee77f31cb35333d9c86c86e12e222a
Rute Berehe <87500572+ruthnixon@users.noreply.github.com>
2021-12-09 18:13:29 -0700

    Delete CSE_310/Project_03 directory

--- CSE_310/Project_03/rp-portfolio/django_app/Personal_Site/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalSiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Personal_Site'
--- CSE_310/Project_03/rp-portfolio/django_app/Personal_Site/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from.models import Post
-
-### This class represents the form that accepts the users input. This form is used in all the html pages.
-
-class PostForm(ModelForm):
-    class Meta:
-        model=Post
-        fields= "__all__"
